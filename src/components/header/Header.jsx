0e7a4b8709f8c2ce9d6f9a6e16b5b968685c63e9
DrissaCAS <Drissa_cas@hotmail.com>
2026-01-30 20:09:02 -0300

    style: adjust header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,20 +4,29 @@ import logo from "../../assets/icones/logo.png"
 
 const Header = () => {
   return (
-    <header>
-        <Link to={"/"}>
-            <img src={logo} alt="Imagem da logo do projeto Connect, sendo representada por uma mão segurando um coração, simbolizando apoio." />
-        </Link>
+    <>
+      <header>
+          <Link to={"/"}>
+              <img src={logo} alt="Imagem da logo do projeto Connect, sendo representada por uma mão segurando um coração, simbolizando apoio." />
+          </Link>
 
-        <nav className={S.navHeader}>
-            <Link to={"/doacao"}>Doação</Link>
-            <Link to={"/voluntariado"}>Voluntariado</Link>
-            <Link to={"/mentoria"}>Mentoria</Link>
-            <Link to={"/eventos"}>Eventos</Link>
-        </nav>
+          <nav className={S.navHeader}>
+              <Link to={"/doacao"} className={S.menu}>Doação</Link>
+              <Link to={"/voluntariado"} className={S.menu}>Voluntariado</Link>
+              <Link to={"/mentoria"} className={S.menu}>Mentoria</Link>
+              <Link to={"/eventos"} className={S.menu}>Eventos</Link>
+          </nav>
 
-        <img src="https://avatars.githubusercontent.com/u/25387693?v=4" alt="Foto do Usuário" className={S.photoProfile} />
-    </header>
+          <img src="https://avatars.githubusercontent.com/u/25387693?v=4" alt="Foto do Usuário" className={S.photoProfile} />
+      </header>
+
+      <nav className={S.navUser}>
+        <Link to={"/usuario"}>Drissa Cas</Link>
+        <Link to={"/voluntariado"}>Meu Voluntariado</Link>
+        <Link>Configurações</Link>
+        <Link className={S.menuMobile}>Sair</Link>
+      </nav>
+    </>
   )
 }
 
