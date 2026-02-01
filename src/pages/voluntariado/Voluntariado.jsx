ac5d674db94aac4d877ba3f94c79db5638c6268e
DrissaCAS <Drissa_cas@hotmail.com>
2026-02-01 20:38:56 -0300

    feat: add cards to volunteers page

--- src/pages/voluntariado/Voluntariado.jsx
+++ src/pages/voluntariado/Voluntariado.jsx
@@ -1,10 +1,12 @@
-
-import React from 'react'
+import CardVoluntariado from "../../components/infocards/CardVoluntariado"
+import "./voluntariado.module.scss"
 
 const Voluntariado = () => {
   return (
     <main>
         <h1>Voluntariado</h1>
+
+        <CardVoluntariado />
     </main>
   )
 }
