c895624135336d7ff73722122e6457db95b809ce
DrissaCAS <Drissa_cas@hotmail.com>
2026-02-01 19:00:34 -0300

    feat: add cards to donation page

--- src/pages/doacao/Doacao.jsx
+++ src/pages/doacao/Doacao.jsx
@@ -1,10 +1,12 @@
-
-
+import CardDoacoes from '../../components/infocards/CardDoacoes'
+import './doacao.module.scss'
 
 const Doacao = () => {
   return (
     <main>
-        <h1>Doação</h1>
+      <h1>Doação</h1>
+
+      <CardDoacoes />
     </main>
   )
 }
