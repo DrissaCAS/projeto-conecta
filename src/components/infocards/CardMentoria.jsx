2211ed3dc0e5a2edc7d9b82d2aaf41fbef1340b8
DrissaCAS <Drissa_cas@hotmail.com>
2026-02-01 18:55:59 -0300

    feat: add mentorship page cards

---
+++ src/components/infocards/CardMentoria.jsx
@@ -0,0 +1,33 @@
+import './infocards.module.scss'
+import img1 from '../../assets/imagens/mentoria/mentoria-carreira-emprego.png'
+import img2 from '../../assets/imagens/mentoria/compartilhe-experiencia.png'
+import img3 from '../../assets/imagens/mentoria/acompanhamento.png'
+
+const CardMentoria = () => {
+    return (
+        <section>
+            <article>
+                <img src={img1} alt="Mentoria de Carreira e Emprego" />
+                <h3>Mentoria de Carreira e Emprego</h3>
+                <p>Orientação sobre currículo, entrevistas e primeiros passos no mercado de trabalho.</p>
+                <button>Quero Participar</button>
+            </article>
+
+            <article>
+                <img src={img2} alt="Projeto Futuro na Escola" />
+                <h3>Compartilhe Experiência</h3>
+                <p>Oriente jovens e profissionais iniciantes em sua área.</p>
+                <button>Quero Participar</button>
+            </article>
+
+            <article>
+                <img src={img3} alt="Acompanhamento" />
+                <h3>Acompanhamento</h3>
+                <p>Participe como guia em jornadas de aprendizado e desenvolvimento.</p>
+                <button>Quero Participar</button>
+            </article>
+        </section>
+    )
+}
+
+export default CardMentoria
