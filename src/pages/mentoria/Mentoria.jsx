4922b2aeed0ae5a15ec307f75c82d9c6bef70495
DrissaCAS <Drissa_cas@hotmail.com>
2026-02-01 18:59:52 -0300

    feat: add cards to mentorship page

--- src/pages/mentoria/Mentoria.jsx
+++ src/pages/mentoria/Mentoria.jsx
@@ -1,10 +1,12 @@
-
-import React from 'react'
+import CardMentoria from "../../components/infocards/CardMentoria"
+import './mentoria.module.scss'
 
 const Mentoria = () => {
   return (
     <main>
         <h1>Mentoria</h1>
+
+        <CardMentoria />
     </main>
   )
 }
