560b8655f19804101f0965b5a54ad3ede6b711fd
DrissaCAS <Drissa_cas@hotmail.com>
2026-02-02 19:34:03 -0300

    feat: implement hamburger menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,23 @@
 import S from "./header.module.scss"
 import { Link } from "react-router-dom"
 import logo from "../../assets/icones/logo.png"
+import { useEffect, useState } from "react"
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
+
+  useEffect(() => {
+    function menuMobile () {
+      setIsMobile(window.innerWidth <= 768);
+      if (innerWidth > 768) {
+        setMenuOpen(false)
+      }
+    }
+    window.addEventListener("resize", menuMobile);
+    return () => window.removeEventListener('resize', menuMobile)
+  }, [])
+
   return (
     <>
       <header>
@@ -10,21 +25,34 @@ const Header = () => {
               <img src={logo} alt="Imagem da logo do projeto Connect, sendo representada por uma mão segurando um coração, simbolizando apoio." />
           </Link>
 
-          <nav className={S.navHeader}>
-              <Link to={"/doacao"} className={S.menu}>Doação</Link>
-              <Link to={"/voluntariado"} className={S.menu}>Voluntariado</Link>
-              <Link to={"/mentoria"} className={S.menu}>Mentoria</Link>
-              <Link to={"/eventos"} className={S.menu}>Eventos</Link>
-          </nav>
+          {!isMobile &&
+            (<nav className={S.navHeader}>
+                <Link to={"/doacao"} className={S.menu} onClick={() => setMenuOpen(false)}>Doação</Link>
+                <Link to={"/voluntariado"} className={S.menu} onClick={() => setMenuOpen(false)}>Voluntariado</Link>
+                <Link to={"/mentoria"} className={S.menu} onClick={() => setMenuOpen(false)}>Mentoria</Link>
+                <Link to={"/eventos"} className={S.menu} onClick={() => setMenuOpen(false)}>Eventos</Link>
+            </nav>)
+          }
 
-          <img src="https://avatars.githubusercontent.com/u/25387693?v=4" alt="Foto do Usuário" className={S.photoProfile} />
+
+          <img className={S.photoProfile} src="https://avatars.githubusercontent.com/u/25387693?v=4" alt="Foto do Usuário"  onClick={() => setMenuOpen(!menuOpen)} />
       </header>
 
-      <nav className={S.navUser}>
-        <Link to={"/usuario"}>Drissa Cas</Link>
-        <Link to={"/voluntariado"}>Meu Voluntariado</Link>
-        <Link>Configurações</Link>
-        <Link className={S.menuMobile}>Sair</Link>
+      <nav className={menuOpen ? S.navUser : S.closedNavUser}>
+        <Link to={"/usuario"} onClick={() => setMenuOpen(false)}>Drissa Cas</Link>
+        <Link to={"/voluntariado"} onClick={() => setMenuOpen(false)}>Meu Voluntariado</Link>
+        <Link onClick={() => setMenuOpen(false)}>Configurações</Link>
+
+        {isMobile &&
+          (<div>
+            <Link to={"/doacao"} className={S.menu} onClick={() => setMenuOpen(false)}>Doação</Link>
+              <Link to={"/voluntariado"} className={S.menu} onClick={() => setMenuOpen(false)}>Voluntariado</Link>
+              <Link to={"/mentoria"} className={S.menu} onClick={() => setMenuOpen(false)}>Mentoria</Link>
+              <Link to={"/eventos"} className={S.menu} onClick={() => setMenuOpen(false)}>Eventos</Link>
+          </div>)
+        }
+
+        <Link className={S.menuMobile} onClick={() => setMenuOpen(false)}>Sair</Link>
       </nav>
     </>
   )
